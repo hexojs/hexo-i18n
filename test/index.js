eeb30d6fc6ee93fac855b1bda680a9401656be77
Tommy Chen <tommy351@gmail.com>
2015-01-09 00:23:03 +0800

    Main program done

--- test/index.js
+++ test/index.js
@@ -1,5 +1,144 @@
 var should = require('chai').should();
 
 describe('i18n', function(){
-  //
+  var ctor = require('../lib/i18n');
+
+  var i18n = new ctor({
+    languages: ['zh-TW', 'en']
+  });
+
+  // Load fixtures
+  i18n.load('en', {
+    ok: 'OK',
+    add: 'Add',
+    name: 'My name is %1$s %2$s.',
+    index: {
+      title: 'Home',
+      video: {
+        zero: 'No videos',
+        one: 'A video',
+        other: '%d videos'
+      }
+    }
+  });
+
+  i18n.load('zh-TW', {
+    add: '新增',
+    name: '我的名字是%2$s%1$s。',
+    index: {
+      title: '首頁',
+      video: {
+        zero: '沒有影片',
+        one: '一部影片',
+        other: '%d 部影片'
+      }
+    }
+  });
+
+  it('constructor', function(){
+    var i18n = new ctor();
+    i18n.languages.should.eql(['default']);
+
+    i18n = new ctor({
+      languages: 'en'
+    });
+
+    i18n.languages.should.eql(['en']);
+
+    i18n = new ctor({
+      languages: ['zh-TW', 'en']
+    });
+
+    i18n.languages.should.eql(['zh-TW', 'en']);
+  });
+
+  it('load()', function(){
+    var i18n = new ctor();
+
+    i18n.load('en', {
+      yes: 'Yes',
+      no: 'No',
+      index: {
+        title: 'Home',
+        author: {
+          name: 'John'
+        }
+      }
+    });
+
+    i18n.data.en.should.eql({
+      yes: 'Yes',
+      no: 'No',
+      'index.title': 'Home',
+      'index.author.name': 'John'
+    });
+  });
+
+  it('_compile() - default languages', function(){
+    var result = i18n._compile();
+
+    result.should.eql({
+      add: '新增',
+      'index.title': '首頁',
+      'index.video.zero': '沒有影片',
+      'index.video.one': '一部影片',
+      'index.video.other': '%d 部影片',
+      name: '我的名字是%2$s%1$s。',
+      ok: 'OK'
+    });
+  });
+
+  it('_compile() - custom languages', function(){
+    var result = i18n._compile('en');
+
+    result.should.eql({
+      add: 'Add',
+      'index.title': 'Home',
+      'index.video.zero': 'No videos',
+      'index.video.one': 'A video',
+      'index.video.other': '%d videos',
+      name: 'My name is %1$s %2$s.',
+      ok: 'OK'
+    });
+  });
+
+  it('__() - default languages', function(){
+    var __ = i18n.__();
+
+    __().should.eql('');
+    __('add').should.eql('新增');
+    __('ok').should.eql('OK');
+    __('index.title').should.eql('首頁');
+    __('name', '大呆', '王').should.eql('我的名字是王大呆。');
+    __('Hello world').should.eql('Hello world');
+  });
+
+  it('__() - custom languages', function(){
+    var __ = i18n.__('en');
+
+    __('add').should.eql('Add');
+    __('ok').should.eql('OK');
+    __('index.title').should.eql('Home');
+    __('name', 'John', 'Doe').should.eql('My name is John Doe.');
+  });
+
+  it('_p() - default languages', function(){
+    var _p = i18n._p();
+
+    _p().should.eql('');
+    _p('ok').should.eql('OK');
+    _p('index.video', 0).should.eql('沒有影片');
+    _p('index.video', 1).should.eql('一部影片');
+    _p('index.video', 10).should.eql('10 部影片');
+    _p('Hello world').should.eql('Hello world');
+  });
+
+  it('_p() - custom languages', function(){
+    var _p = i18n._p('en');
+
+    _p('ok').should.eql('OK');
+    _p('index.video', 0).should.eql('No videos');
+    _p('index.video', 1).should.eql('A video');
+    _p('index.video', 10).should.eql('10 videos');
+  });
 });
